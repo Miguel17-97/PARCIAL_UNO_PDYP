c30d6f016a027aa03bd37025d73a3ba0189e8396
miguelangel <cifuentesmiguelangel97@gmail.com>
2024-04-09 13:44:40 -0500

    Proyecto agregado al repositorio

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.courses(id, category, name, teacher) VALUES (1, 'Ciencias exactas', 'Matematicas', 'Juan Diego Ossa');
+
+INSERT INTO public.courses(id, category, name, teacher) VALUES (2, 'Biologia', 'Ciencias', 'Leandro Jarammillo');
+
+INSERT INTO public.courses(id, category, name, teacher) VALUES (3, 'Tecnologia', 'Programacion', 'Neiro Figueroa');
+
+
+INSERT INTO public.students(id, age, last_name, name, course_id) VALUES (1, '26', 'Serna', 'John', 1);
+INSERT INTO public.students(id, age, last_name, name, course_id) VALUES (2, '21', 'Calle', 'Andrea', 2);
+INSERT INTO public.students(id, age, last_name, name, course_id) VALUES (3, '22', 'Hoyos', 'Anyi', 1);
+INSERT INTO public.students(id, age, last_name, name, course_id) VALUES (4, '24', 'Perez', 'Santiago', 3);
